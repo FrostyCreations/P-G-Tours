f093aaf942666194986761e5482d0cd2c697e003
FrostyCreations <francois.theron801@gmail.com>
2026-03-19 15:58:17 +0200

    Hardcode server IP to fix deployment

--- src/pages/QuotesPage.jsx
+++ src/pages/QuotesPage.jsx
@@ -1,3 +1,4 @@
+// v1.1.1 - Triggering xneelo deployment
 import React, { useMemo, useState } from 'react';
 import { useEditor } from '../context/EditorContext';
 import { Link } from 'react-router-dom';
